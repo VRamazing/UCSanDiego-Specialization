d4594131fc36ef02a2a4b46b1aa42622a61942f9
VRamazing <r.vignesh149@gmail.com>
2017-06-20 23:30:22 +0530

    Signature partial

--- Assignment 3/greedy_algorithms_starter_files/covering_segments/covering_segments.py
+++ Assignment 3/greedy_algorithms_starter_files/covering_segments/covering_segments.py
@@ -5,12 +5,35 @@ from collections import namedtuple
 Segment = namedtuple('Segment', 'start end')
 
 def optimal_points(segments):
+
     points = []
+    final = []
+    
     #write your code here
+
     for s in segments:
         points.append(s.start)
         points.append(s.end)
-    return points
+
+    points.sort()    
+    dictPoints={}
+    lenPoints=len(points)
+
+    for i in range(points[0],points[lenPoints-1]+1):
+        dictPoints[i] = 0
+        for s in segments:
+            if i >= s.start and i <= s.end:
+                dictPoints[i] += 1
+    
+    list1=list(dictPoints.values())
+    list1.sort(reverse = True)
+    
+    #Find the minimum num points that satisfies all lines.
+
+
+    print(dictPoints)
+    print(list1)
+    return final
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- Assignment 3/greedy_algorithms_starter_files/dot_product/dot_product.py
+++ Assignment 3/greedy_algorithms_starter_files/dot_product/dot_product.py
@@ -2,6 +2,22 @@
 
 import sys
 
+# Problem Introduction
+# You have n ads to place on a popular Internet page. For each ad, you know how
+# much is the advertiser willing to pay for one click on this ad. You have set up n
+# slots on your page and estimated the expected number of clicks per day for each
+# slot. Now, your goal is to distribute the ads among the slots to maximize the
+# total revenue.
+
+
+# Task. Given two sequences a 1 , a 2 , . . . , a n (a i is the profit per click of the i-th ad) and b 1 , b 2 , . . . , b n (b i is
+# the average number of clicks per day of the i-th slot), we need to partition them into n pairs (a i , b j )
+# such that the sum of their products is maximized.
+# Input Format. The first line contains an integer n, the second one contains a sequence of integers
+# a 1 , a 2 , . . . , a n , the third one contains a sequence of integers b 1 , b 2 , . . . , b n .
+# Constraints. 1 ≤ n ≤ 10 3 ; −10 5 ≤ a i , b i ≤ 10 5 for all 1 ≤ i ≤ n.
+
+
 def max_dot_product(a, b):
     #write your code here
     # print("A,B %s %s" % (a,b)) 
